dada0b5e59f1bde7197ae9a023d837e6c07db80f
czekk <ckunaraja@njcu.edu>
2020-09-09 23:05:20 -0400

    added code to store in local storage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import Tasks from './Components/Tasks/Tasks';
 import Header from './Components/Header/Header';
 
 class App extends Component {
-  state= {
+  //state= ()=>this.retrieveState();
+  /*{
     tasks: [
     ]
+  }*/
+
+  storeState= () =>{ 
+    const stateStr = JSON.stringify(this.state);
+    localStorage.setItem("state", stateStr);
   }
+
+  state= () => {
+    const stateStr = localStorage.getItem("state");
+    const updatedState= null;
+    if(stateStr != null) {
+            const retrievedState= JSON.parse(stateStr);
+            updatedState= retrievedState.tasks;
+      }
+    else {
+            const newState= {tasks: []};
+            updatedState= newState;
+          }
+          return updatedState;
+    }
 
   addNewTaskHandler =() =>{
     const tasks = [...this.state.tasks];
@@ -20,6 +40,7 @@ class App extends Component {
     };
     tasks.push(NewTask);
     this.setState({tasks: tasks});
+    this.storeState();
   }
   changeFieldsHandler=(event, taskIndex) =>{
     const tasks = [...this.state.tasks];
@@ -27,11 +48,13 @@ class App extends Component {
     let  val= event.target.value;
     tasks[taskIndex][nam]= val;
     this.setState({tasks: tasks});
+    this.storeState();
   }
   deleteTaskHandler =(taskIndex) =>{
     let tasks =[...this.state.tasks];
     tasks.splice( taskIndex, 1 );
     this.setState({tasks: tasks});
+    this.storeState();
   }
   render() {
     let tasks = null;
